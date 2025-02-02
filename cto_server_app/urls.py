6fccaa81a5f083b3d89ad4c6a8f92fc6230dbf12
ItsOlegDm <itsolegdm@gmail.com>
2025-02-02 22:52:49 +0200

    Revert "add contact_form app"
    This reverts commit 402d0b803d459016d8d523b2b3f65b0a52611c34.

--- cto_server_app/urls.py
+++ cto_server_app/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/blog/', include('blog.urls')),
-    path('api/v1/contact_form/', include('contact_form.urls')),
 ]
