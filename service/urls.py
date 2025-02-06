19db245271d6bcb609e118cc4e232e944090b181
IrenaPavlikha <kotova.dream@gmail.com>
2025-02-06 23:08:42 +0200

    07022025

--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from . import views
 
+from . import views
+from django.urls import include, path
 urlpatterns = [
     path('', views.all_services, name='all_services'),
     path('<int:pk>/', views.service_detail, name='service_detail'),
+    path('api/', include('telegram_api.urls')),
 ]
