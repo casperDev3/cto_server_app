ebcebc319c8e3d4ff236570539806b4f6548e564
Ihor Lialiuk <ihor.lialiuk@gmail.com>
2025-02-06 19:44:07 +0200

    fix path for reviews from a1d

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reviews/',views.all_reviews, name='reviews-all'),
-    path('reviews/<int:pk>/', views.reviews_detail, name='reviews-detail'),
+    path('',views.all_reviews, name='reviews-all'),
+    path('<int:pk>/', views.reviews_detail, name='reviews-detail'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -9,6 +9,7 @@ from .models import Review
 from .utils.sorting import Sorting
 from .utils.processing import Processing
 
+
 @require_http_methods(["GET", "POST"])
 def all_reviews(request):
     try:
@@ -39,32 +40,33 @@ def all_reviews(request):
             return Helpers.success_created(reviews)  # Передаём объект, но обрабатываем его в хелпере
     except KeyError as e:
         return Helpers.internal_server_error(f"This field does not exist in the Rating table: {str(e)}",
-                                                 status=400)
+                                             status=400)
     except Exception as e:
         return Helpers.internal_server_error(str(e))
-    #0602
-
-    @require_http_methods(["PUT", "DELETE"])
-    def reviews_detail(request, pk):
-        reviews = get_object_or_404(Review, pk=pk)
-        try:
-            if request.method == "PUT":
-
-                try:
-                    data = json.loads(request.body)  # Для raw JSON
-                except json.JSONDecodeError:
-                    return Helpers.internal_server_error("Некорректный JSON")
-
-                    # Проверяем и обновляем через сериализатор
-                serializer = ReviewSerializer(reviews, data=data, partial=True)
-                if serializer.is_valid():
-                    serializer.save()
-                return Helpers.success_updated(reviews)
+    # 0602
 
 
-            elif request.method == "DELETE":
-                reviews = get_object_or_404(Review, pk=pk)
-                reviews.delete()
-                return Helpers.success_deleted(reviews)
-        except Exception as e:
-            return JsonResponse({"data": None, "meta": {}}, status=500)
+@require_http_methods(["PUT", "DELETE"])
+def reviews_detail(request, pk):
+    reviews = get_object_or_404(Review, pk=pk)
+    try:
+        if request.method == "PUT":
+
+            try:
+                data = json.loads(request.body)  # Для raw JSON
+            except json.JSONDecodeError:
+                return Helpers.internal_server_error("Некорректный JSON")
+
+                # Проверяем и обновляем через сериализатор
+            serializer = ReviewSerializer(reviews, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+            return Helpers.success_updated(reviews)
+
+
+        elif request.method == "DELETE":
+            reviews = get_object_or_404(Review, pk=pk)
+            reviews.delete()
+            return Helpers.success_deleted(reviews)
+    except Exception as e:
+        return JsonResponse({"data": None, "meta": {}}, status=500)
