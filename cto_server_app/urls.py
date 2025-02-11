0c897f4f255e3c83151ae1b35750c23bbfcfa6d2
ItsOlegDm <itsolegdm@gmail.com>
2025-02-11 20:16:15 +0200

    change path for contuct form

--- cto_server_app/urls.py
+++ cto_server_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/blog/', include('blog.urls')),
-    path('api/v1/contact_form/', include('contact_form.urls')),
+    path('api/v1/forms/contact_us/', include('contact_form.urls')),
 ]
